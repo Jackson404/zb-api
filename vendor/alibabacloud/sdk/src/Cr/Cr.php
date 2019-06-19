311df9dd5bd5c9955007d612f202398be1124998
xll <xuliulei@zhengbu121.com>
2019-06-19 16:35:25 +0800

    简历管理

---
+++ vendor/alibabacloud/sdk/src/Cr/Cr.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AlibabaCloud\Cr;
+
+use AlibabaCloud\Client\Resolver\VersionResolver;
+
+/**
+ * @method static V20160607\CrApiResolver v20160607()
+ * @method static V20181201\CrApiResolver v20181201()
+ */
+class Cr extends VersionResolver
+{
+}
