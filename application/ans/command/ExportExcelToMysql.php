2a164f3cae30bbc652c38e9b56965f250f8e8f23
xll <xuliulei@zhengbu121.com>
2019-07-19 18:10:20 +0800

    简历增加求职意向

--- application/ans/command/ExportExcelToMysql.php
+++ application/ans/command/ExportExcelToMysql.php
@@ -137,24 +137,45 @@ class ExportExcelToMysql extends Command
 //                ];
 
 
+//                $arr = [
+//                    'name' => $v[0] == null ? '' : $v[0],
+//                    'position' => $v[1] == null ? '' : $v[1],
+//                    'resume' => $v[2] == null ? '' : $v[2],
+//                    'sex' => $v[3] == null ? '' : $v[3],
+//                    'birth' => $v[4] == null ? '' : $v[4],
+//                    'work' => $v[5] == null ? '' : $v[5],
+//                    'phone' => $v[6] == null ? '' : $v[6],
+//                    'mail' => $v[7] == null ? '' : $v[7],
+//                    'habitation' => $v[8] == null ? '' : $v[8],
+//                    'profe' => $v[9] == null ? '' : $v[9],
+//                    'profession' => $v[10] == null ? '' : $v[10],
+//                    'gra' => $v[11] == null ? '' : $v[11],
+//                    'spe' => $v[12] == null ? '' : $v[12],
+//                    'qua' => $v[13] == null ? '' : $v[13],
+//                    'wage' => $v[14] == null ? '' : $v[14],
+//                    'from' => '智联招聘',
+//                    'createTime' => $v[15] == null ? '2019-07-19' : $v[15]
+//                ];
+
                 $arr = [
-                    'name' => $v[0] ?? '',
-                    'position' => $v[1] ?? '',
-                    'resume' => $v[2] ?? '',
-                    'sex' => $v[3] ?? '',
-                    'birth' => $v[4] ?? '',
-                    'work' => $v[5] ?? '',
-                    'phone' => $v[6] ?? '',
-                    'mail' => $v[7] ?? '',
-                    'habitation' => $v[8] ?? '',
-                    'profe' => $v[9] ?? '',
-                    'profession' => $v[10] ?? '',
-                    'gra' => $v[11] ?? '',
-                    'spe' => $v[12] ?? '',
-                    'qua' => $v[13] ?? '',
-                    'wage' => $v[14] ?? '',
+                    'name' => $v[0] == null ? '' : $v[0],
+                    'sex' => $v[3] == null ? '' : $v[3],
+                    'position' => $v[1] == null ? '' : $v[1],
+                    'resume' => $v[2] == null ? '' : $v[2],
+
+                    'birth' => $v[4] == null ? '' : $v[4],
+                    'work' => $v[5] == null ? '' : $v[5],
+                    'phone' => $v[6] == null ? '' : $v[6],
+                    'mail' => $v[7] == null ? '' : $v[7],
+                    'habitation' => $v[8] == null ? '' : $v[8],
+                    'profe' => $v[9] == null ? '' : $v[9],
+                    'profession' => $v[10] == null ? '' : $v[10],
+                    'gra' => $v[11] == null ? '' : $v[11],
+                    'spe' => $v[12] == null ? '' : $v[12],
+                    'qua' => $v[13] == null ? '' : $v[13],
+                    'wage' => $v[14] == null ? '' : $v[14],
                     'from' => '智联招聘',
-                    'createTime' => $v[15]
+                    'createTime' => $v[15] == null ? '2019-07-19' : $v[15]
                 ];
                 var_dump($arr);
                 array_push($sqlData, $arr);
@@ -166,6 +187,6 @@ class ExportExcelToMysql extends Command
 
         }
 
-        var_dump($db->table('resume_zl')->insertAll($sqlData));
+         var_dump($db->table('resume_zl')->insertAll($sqlData));
     }
 }
