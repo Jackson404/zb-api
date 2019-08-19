e552980e2c529362f0a26ecfeebb3e8a34878582
Your Name <you@example.com>
2019-08-19 15:20:07 +0800

    8-19

--- thinkphp/library/think/cache/driver/Redis.php
+++ thinkphp/library/think/cache/driver/Redis.php
@@ -25,7 +25,7 @@ class Redis extends Driver
     protected $options = [
         'host'       => '127.0.0.1',
         'port'       => 6379,
-        'password'   => 'zhengbu123',
+        'password'   => '',
         'select'     => 0,
         'timeout'    => 0,
         'expire'     => 0,
