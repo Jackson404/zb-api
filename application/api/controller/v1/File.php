654f933770800bbfe92b450409af287c4cf9bbb5
xuliulei <xuliulei@zhengbu121.com>
2019-08-20 18:10:35 +0800

    oss接口修改

--- application/api/controller/v1/File.php
+++ application/api/controller/v1/File.php
@@ -34,13 +34,32 @@ class File extends AuthBase
         }
 
         $tmpInfo = $file->getInfo();
+        //$type = $tmpInfo['type'];
+        //$typeArr = explode('/',$type);
         $object = $tmpInfo['name'];
+
+//        $nowDate = date('Ymd',time());
+//        $to_object = Util::generateRandomCode(22).'.'.$typeArr[1];
         $filepath = $tmpInfo['tmp_name'];
 
         try {
             $ossClient = new OssClient($GLOBALS['ACCESS_KEY_ID'], $GLOBALS['ACCESS_KEY_SECRET'], $GLOBALS['ENDPOINT']);
+
+//            // 创建文件夹
+//            $ossClient->createObjectDir($GLOBALS['BUCKET'],$nowDate);
+//
+//            $exist = $ossClient->doesObjectExist($GLOBALS['BUCKET'], $object);
+//            if ($exist){
+//                $ossClient->copyObject($GLOBALS['BUCKET'], $object, $GLOBALS['BUCKET'], $to_object);
+//                echo 'exist';
+//            }
+//            exit;
+
             $res = $ossClient->uploadFile($GLOBALS['BUCKET'], $object, $filepath);
+
             $data['imgUrl'] = 'https://' . $GLOBALS['PIC_SERVER'] . '/' . $object;
+            var_dump($object);
+            var_dump($res);
             Util::printResult($GLOBALS['ERROR_SUCCESS'], $data);
             exit;
         } catch (OssException $e) {
