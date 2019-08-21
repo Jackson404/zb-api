5c548acce764f5d6c43fd9f4e4738657ba196a0a
xuliulei <xuliulei@zhengbu121.com>
2019-08-21 18:13:35 +0800

    审核员工

---
+++ application/api/controller/v1/epOffline/EpUser.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\api\controller\v1\epOffline;
+
+use app\api\controller\v1\ep\EpUserBase;
+
+class EpUser extends EpUserBase
+{
+    public function addUser()
+    {
+
+    }
+}
+
